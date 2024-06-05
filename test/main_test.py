7644dfeb72a080e5f381123c11efc1669a9045fe
Dmitry Glyzin <glyzin@gmail.com>
2024-06-05 09:53:40 +0000

    first trainig test

--- test/main_test.py
+++ test/main_test.py
@@ -1,7 +1,9 @@
 import ancinf
 
 def test_helook():
-    #python3 -m ancinf preprocess ../datasets/ ./test5/ --infile simple.ancinf
+    # python3 -m ancinf preprocess ../datasets/ ./test5/ --infile simple.ancinf
     ancinf.preprocess_fn("./test/datasets/", "./test/manual/test5/", "simple.ancinf", None, 2023)
-    #print('dir(ancinf)')
+    # python3 -m ancinf crossval ./test5/ --infile simple.explist 
+    # workdir, infile, outfile, seed, processes, fromexp, toexp, fromsplit, tosplit, gpu, gpucount
+    ancinf.crossval_fn("./test/manual/test5/", "simple.explist", None, 2023, 1, None, None, None, None, 0, 1)
     assert True 
